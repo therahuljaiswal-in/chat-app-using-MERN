f1a3e218c1689b27a2faab0fa79904a2437c1272
Rahul Jaiswal <168125798+therahuljaiswal-in@users.noreply.github.com>
2024-08-29 19:36:25 +0530

    Update Sidebar.jsx

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import SearchInput from './SearchInput'
 import Conversations from './Conversations'
-import LogOutButton from './LogOutButton'
+import LogoutButton from './LogoutButton'
 
 const Sidebar = () => {
   return (
@@ -9,7 +9,7 @@ const Sidebar = () => {
       <SearchInput/>
       <div className='divider px-3'></div>
       <Conversations />
-      <LogOutButton /> 
+      <LogoutButton /> 
     </div>
   )
 }
